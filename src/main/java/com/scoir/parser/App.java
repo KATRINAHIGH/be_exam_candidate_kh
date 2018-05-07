8b11a6cc785acc49e5167e403c6f39cb97f06484
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-05-07 19:50:39 -0400

    wiring everything up to get the program up and running

--- src/main/java/com/scoir/parser/App.java
+++ src/main/java/com/scoir/parser/App.java
@@ -1,8 +1,27 @@
 package com.scoir.parser;
 
+import com.scoir.parser.services.ConfigService;
+import com.scoir.parser.services.Csv2JsonService;
+import com.scoir.parser.services.FileSystemService;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        //instantiate services
+        ConfigService cs = new ConfigService();
+        FileSystemService fss = new FileSystemService();
+        Csv2JsonService csv = new Csv2JsonService();
+
+        //set service contracts
+        fss.setCsvProcessor(csv); //dependency injection
+
+        //set up and run
+        //load config
+        cs.load("config.properties");
+
+        //watch folders for changes
+        fss.ensureDirectoriesExist();
+
+        fss.watchForChanges();
     }
 }
