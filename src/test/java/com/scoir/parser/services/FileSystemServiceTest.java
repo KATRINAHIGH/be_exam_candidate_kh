332ffb79476f657e9ca1d4d9d21c65466d3859df
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-05-07 12:40:11 -0400

    stubbed test file for watchForChanges

---
+++ src/test/java/com/scoir/parser/services/FileSystemServiceTest.java
@@ -0,0 +1,20 @@
+package com.scoir.parser.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileSystemServiceTest {
+
+
+    @Before
+    public void setUp() {
+    }
+
+    //https://stackoverflow.com/questions/29719932/unit-test-code-with-watchservice
+    //TODO: Will require mocking of the csv service
+    @Test
+    public void watchForChanges() {
+    }
+}
