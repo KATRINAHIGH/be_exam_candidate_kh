c4fcffdf934ce8dd0d97b2bdd6e0384ea9dabf50
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-05-07 13:47:20 -0400

    created config and global class to create environment variables from paths

---
+++ src/main/java/com/scoir/parser/Globals.java
@@ -0,0 +1,8 @@
+package com.scoir.parser;
+
+public class Globals {
+    //created constants they never change, can call upon without instantiating
+    public static final String InputPath = "inputpath";
+    public static final String OutputPath = "outputpath";
+    public static final String ErrorPath = "errorpath";
+}
