2c7f48cb28d918e908aed86a9c9bdd23de754b01
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-05-07 18:09:35 -0400

    mapped csv to record pojo

--- src/main/java/com/scoir/parser/models/CsvRecord.java
+++ src/main/java/com/scoir/parser/models/CsvRecord.java
@@ -1,6 +1,34 @@
 package com.scoir.parser.models;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "first", "middle", "last", "phone"}) //https://gist.github.com/asafary/9675488
 public class CsvRecord {
+
+    public String id;
+    public String first;
+    public String middle;
+    public String last;
+    public String phone;
+
+    /**
+     *creates an object from the csv file and converts it to a record. Allowing the nesting of name.
+     * @return a Record object with properly nested name fields
+     */
+    public Record toRecord(){
+        Record record = new Record();
+        Name name = new Name();
+
+        record.setId(this.id);
+        record.setPhone(this.phone);
+        name.setFirst(this.first);
+        name.setMiddle(this.middle);
+        name.setLast(this.last);
+        record.setName(name);
+
+        return record;
+    }
 }
+
 
 
